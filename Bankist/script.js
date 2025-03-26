817676df54887a95c42b660d3ebe7ee387c50ed1
Aarif <adebayoabdulazeez195@gmail.com>
2025-03-26 11:17:27 -0700

    css style behavour adjustment

--- Bankist/script.js
+++ Bankist/script.js
@@ -12,6 +12,9 @@ const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
 console.log(tabs, tabsContainer);
 
+console.log(modal.getBoundingClientRect());
+
+
 ///////////////////////////////////////
 // Modal window
 
